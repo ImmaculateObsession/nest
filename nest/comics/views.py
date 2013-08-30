892d892e7a42cd5bbf100bcd16c98ee60133ce53
Philip James <pjj@philipjohnjames.com>
2013-08-29 21:55:34 -0700

    Stopping for the night.

--- nest/comics/views.py
+++ nest/comics/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from comics.models import (
@@ -9,7 +10,7 @@ class HomeView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
-        context = super(TemplateView, self).get_context_data(**kwargs)
+        context = super(HomeView, self).get_context_data(**kwargs)
         comic = Comic.objects.latest('published')
         context['comic'] = comic
         if comic.post:
@@ -19,4 +20,27 @@ class HomeView(TemplateView):
                 context['post'] = Post.objects.latest('published')
             except Post.DoesNotExist:
                 pass
+
+        context['first_comic'] = Comic.objects.filter(published__lt=comic.published).order_by('published')[0]
         return context
+
+class ComicPostView(TemplateView):
+    template_name = "comic.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ComicView, self).get_context_data(**kwargs)
+        post = get_object_or_404(Post, slug=self.kwargs['slug'])
+        comic = Comic.objects.get(post=post)
+
+        context['post'] = post
+        context['comic'] = comic
+
+        context['first_comic'] = Comic.objects.filter(published__lt=comic.published).order_by('published')[0]
+        context['last_comic'] = Comic.objects.latest('published')
+        context['previous'] = Comic.objects.filter(published__lt=comic.published).order_by('-published')[0]
+        context['next'] = Comic.objects.filter(published__gt=comic.published).order_by('published')[0]
+
+        return context
+
+class ComicDetailView(TemplateView):
+    pass
--- nest/nest/urls.py
+++ nest/nest/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from comics.views import HomeView
+from comics.views import (
+    HomeView,
+    ComicPostView,
+)
 
 admin.autodiscover()
 
@@ -9,6 +12,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^comic/(?P<slug>[\w-]+)/$', ComicPostView.as_view(), name='comicpostview'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
