0a2285ac398a237a746fed11abca78fcb8c75252
Philip James <pjj@philipjohnjames.com>
2013-09-10 17:30:45 -0700

    Add db settings for angel.

--- nest/settings/heroku.py
+++ nest/settings/heroku.py
@@ -4,7 +4,19 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-DATABASES['default'] = dj_database_url.config()
+try:
+    DATABASES['default'] = dj_database_url.config()
+except ImproperlyConfigured:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'postgresql_psycopg2',
+            'NAME': 'quailcomics',
+            'USER': 'quailcomics',
+            'PASSWORD': 'quailcomics',
+            'HOST': '192.241.228.250',
+            'PORT': '49158',
+        }
+    }
 
 ALLOWED_HOSTS = [
     '.captainquail.com',
