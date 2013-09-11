900b45c49573ee4fbaacf65fd9c02adef87639b1
Philip James <pjj@philipjohnjames.com>
2013-09-10 22:06:38 -0700

    Undo that which was done.

--- nest/settings/heroku.py
+++ nest/settings/heroku.py
@@ -4,19 +4,7 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-try:
-    DATABASES['default'] = dj_database_url.config()
-except ImproperlyConfigured:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'postgresql_psycopg2',
-            'NAME': 'quailcomics',
-            'USER': 'quailcomics',
-            'PASSWORD': 'quailcomics',
-            'HOST': '192.241.228.250',
-            'PORT': '49158',
-        }
-    }
+DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = [
     '.captainquail.com',
