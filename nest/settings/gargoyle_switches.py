41ec9e792dda3a6ae763bef921e6eecc6dee20da
Philip James <pjj@philipjohnjames.com>
2013-10-26 20:31:02 -0700

    Actually put the switch settings in the repo, maybe.

---
+++ nest/settings/gargoyle_switches.py
@@ -0,0 +1,7 @@
+GARGOYLE_SWITCH_DEFAULTS = {
+    'show_ads': {
+        'is_active': False,
+        'label': 'Show ads on the site',
+        'description': 'Whether or not ads are displayed.',
+    },
+}
