478030d84e1ed9bc1f2db44073e8d22907103b77
Philip James <pjj@philipjohnjames.com>
2013-11-14 07:54:34 -0800

    Fix subscribe form submission.

--- static/sass/javascripts/subscribe_popover.js
+++ static/sass/javascripts/subscribe_popover.js
@@ -2,13 +2,12 @@ window.SubscribePopover = (function ($) {
     function handleSubscription() {
         $('#subscribe-btn').popover('hide');
         $('#popover-action').removeClass('fa fa-times').addClass('caret');
-        $('#mc-embedded-subscribe-form').submit();
     }
 
     function handleSubscribeOpen() {
         $('#popover-action').toggleClass('caret').toggleClass('fa fa-times');
         $("#subscribe-btn").popover();
-        $('#subscribe-submit-btn').on('click', handleSubscription);
+        $('#popover-subscription-form').on('submit', handleSubscription);
     }
 
     return {
@@ -17,12 +16,12 @@ window.SubscribePopover = (function ($) {
             title: 'Sign up for weekly updates!',
             html: 'true',
             placement: 'bottom',
-            content: '<form action="http://inkpebble.us3.list-manage1.com/subscribe/post?u=0591f32c13358b969bde9f7c9&amp;id=274876cee0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>\
+            content: '<form action="http://inkpebble.us3.list-manage1.com/subscribe/post?u=0591f32c13358b969bde9f7c9&amp;id=274876cee0" method="post" id="popover-subscription-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>\
                 <div class="form-group">\
                 <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="email address" required>\
                 </div>\
-                </form>\
-                <button id="subscribe-submit-btn" class="btn btn-default">Get weekly updates!</button>'
+                <input type="submit" value="Get weekly updates!" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default">\
+                </form>'
             });
             $('#subscribe-btn').on('click', handleSubscribeOpen);
         }
