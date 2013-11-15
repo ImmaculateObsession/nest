60665072dc0c0a57002aab125b6c01a300a2942e
Philip James <pjj@philipjohnjames.com>
2013-11-15 11:30:49 -0800

    Remove petroglyphs app, add requirements line for it.

--- petroglyphs/context_processors.py
+++
@@ -1,9 +0,0 @@
-from petroglyphs.models import Setting
-
-def setting_injector(request):
-
-    context_dict = {}
-    for setting in Setting.objects.filter(export_to_template=True):
-        context_dict[setting.key.upper()] = str(setting.value)
-
-    return context_dict
