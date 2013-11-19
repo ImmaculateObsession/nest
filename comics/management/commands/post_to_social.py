aab9c496dc4324526883ee7658a3d9422e22bb09
Philip James <pjj@philipjohnjames.com>
2013-11-19 11:05:57 -0800

    Add twitter to post_to_social command.

--- comics/management/commands/post_to_social.py
+++ comics/management/commands/post_to_social.py
@@ -1,13 +1,15 @@
 import logging
+import tweepy
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from allauth.socialaccount.models import SocialToken
+from allauth.socialaccount.models import SocialToken, SocialApp
 from facepy import GraphAPI
 
 from comics.models import SocialPost
 from comics import settings as site_settings
 
 logger = logging.getLogger('post_to_social')
+
 
 class Command(BaseCommand):
     help = "Posts new comics to social networks"
@@ -17,6 +19,9 @@ class Command(BaseCommand):
             posted=False,
             time_to_post__lte=timezone.now(),
         )
+        twitter_app_creds = SocialApp.objects.filter(provider='twitter')[0]
+        twitter_key = twitter_app_creds.client_id
+        twitter_secret = twitter_app_creds.secret
         for post in potential_posts:
             user = post.user
             if post.comic:
@@ -31,7 +36,7 @@ class Command(BaseCommand):
                         app__provider='facebook'
                     ).token
                 except:
-                    logger.exception('No matching social token for %s!' % (post.social_network,))
+                    logger.exception('Error getting social token for %s!' % (post.social_network,))
                 if token:
                     try:
                         graph = GraphAPI(token)
@@ -39,6 +44,23 @@ class Command(BaseCommand):
                             path='%s/links' % (site_settings.facebook_page_id()),
                             link=url,
                         )
+                    except:
+                        logger.exception('Post to %s failed' % (post.social_network,))
+            if post.social_network == 'twitter':
+                token = None
+                try:
+                    token = SocialToken.objects.get(
+                        account__user=user,
+                        app__provider='twitter'
+                    )
+                except:
+                    logger.exception('Error getting social token for %s!' % (post.social_network,))
+                if token:
+                    try:
+                        auth = tweepy.OAuthHandler(twitter_key, twitter_secret)
+                        auth.set_access_token(token.token, token.token_secret)
+                        api = tweepy.API(auth)
+                        api.update_status('Test!')
                     except:
                         logger.exception('Post to %s failed' % (post.social_network,))
             logger.info('Post to %s successful' % (post.social_network,))
