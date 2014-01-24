cba09c0c856e82a25511ab16bd46d30bb73f4560
Philip James <pjj@philipjohnjames.com>
2014-01-23 17:10:00 -0800

    Add mixpanel tracking to RSS feeds

--- comics/feeds.py
+++ comics/feeds.py
@@ -36,5 +36,11 @@ class LatestPostFeed(Feed):
     def get_feed(self, obj, request):
         self.request = request
         feed = super(LatestPostFeed, self).get_feed(obj, request)
-
+        mp = Mixpanel(Setting.objects.get(key='mixpanel_key').value)
+        mp.track('rss', 'rss_hit', {
+            'pebble_id': self.request.pebble.id,
+            'user_agent': request.META.get('HTTP_USER_AGENT', 'none'),
+            'remote_addr': request.META.get('REMOTE_ADDR', 'none'),
+            'http_host': request.META.get('HTTP_HOST', 'none'),
+        })
         return feed
