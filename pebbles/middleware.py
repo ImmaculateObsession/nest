a6bbc4d9c9b25f6a72186582582da3702f806841
Philip James <pjj@philipjohnjames.com>
2014-02-06 22:47:34 -0800

    Remove whitelist from middleware

--- pebbles/middleware.py
+++ pebbles/middleware.py
@@ -10,12 +10,10 @@ from pebbles.models import (
 )
 
 class PebbleMiddleware(object):
-    whitelist = ['/admin']
 
     def process_request(self, request):
         domain = request.META.get('HTTP_HOST').split(':')[0]
-        if any(substring in request.path for substring in self.whitelist):
-            return
+
         try:
             pebble = Domain.objects.get(url=domain).pebble
         except Domain.DoesNotExist:
