7178bc97274b5b47e75751b90ee00d2ef0960262
Philip James <pjj@philipjohnjames.com>
2014-02-16 12:00:31 -0800

    Connect the contributor line under the comic to the profile page.

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.db import models
 from django.db.models.signals import post_save
+from django.core.urlresolvers import reverse
+
 
 from comics.models import (
     Comic,
@@ -16,6 +19,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return "%s's Profile" % (self.user.username)
+
+    def profile_url(self):
+        domain = Site.objects.get_current().domain
+        return "http://%s%s" % (
+            domain,
+            reverse('publicprofileview', args=(self.user.username,))
+        )
 
 
 def create_profile(sender, instance, created, **kwargs):
--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import patterns, include, url
 from profiles.views import ProfileView
 
 profilepatterns = patterns('',
-    url(r'^(?P<username>[\w-]+)/$', ProfileView.as_view()),
+    url(
+        r'^(?P<username>[\w-]+)/$',
+        ProfileView.as_view(),
+        name='publicprofileview',
+    ),
     url(r'^$', ProfileView.as_view(), name='profileview'),
 )
