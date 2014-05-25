55ed3c2acefd8532830b15978e7f007061d54a58
Philip James <pjj@philipjohnjames.com>
2014-05-25 14:37:45 -0700

    Fix feed item urls.

--- comics/feeds.py
+++ comics/feeds.py
@@ -13,10 +13,10 @@ class LatestPostFeed(Feed):
     description_template = "postfeed.html"
 
     def description(self):
-        return self.pebble_settings.get('feed_description', 'Comic Feed')
+        return self.pebble_settings.settings.get('feed_description', 'Comic Feed')
 
     def title(self):
-        return self.pebble_settings.get('feed_title', self.pebble.title)
+        return self.pebble_settings.settings.get('feed_title', self.pebble.title)
 
     def items(self):
         return Post.published_posts.filter(pebbles=self.pebble).order_by('-published')[:5]
@@ -25,9 +25,10 @@ class LatestPostFeed(Feed):
         return item.title
 
     def item_link(self, item):
+        url = self.pebble_settings.primary_domain.url
         if item.comic:
-            return reverse('comicpostview', args=[item.slug])
-        return reverse('postview', args=[item.slug])
+            return 'http://' + url + reverse('comicpostview', args=[item.slug])
+        return 'http://' + url + reverse('postview', args=[item.slug])
 
     def item_pubdate(self, item):
         return item.published
@@ -37,7 +38,7 @@ class LatestPostFeed(Feed):
         self.pebble = self.request.pebble
         if not self.pebble:
             raise Http404()
-        self.pebble_settings = PebbleSettings.objects.get(pebble=self.pebble).settings
+        self.pebble_settings = PebbleSettings.objects.get(pebble=self.pebble)
         feed = super(LatestPostFeed, self).get_feed(obj, request)
         mp = Mixpanel(Setting.objects.get(key='mixpanel_key').value)
         mp.track('rss', 'rss_hit', {
